0526b1ecb05372763808e759b7d30cfa375a4151
EngMonicaMohammadi <monicamohammadi456@gmail.com>
2021-03-24 10:41:57 +0430

    video3

--- App.js
+++ App.js
@@ -1,16 +1,58 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator} from '@react-navigation/stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 import Home from './Screen/Home';
+const Stack = createStackNavigator()
+const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
-    <View >
-      <Home  />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Food"
+        tabBarOptions={{
+         activeTintColor:'#841548'
+       }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={HomeStack}
+          options={{
+            tabBarLabel:"Home",
+            tabBarIcon:({color,size}) => (
+                <MaterialCommunityIcons
+                  name="home"
+                  color={color}
+                  size={size}
+                />
+            )
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
+function HomeStack (){
+  return (
+    <Stack.Navigator 
+    initialRouteName="Home"
+    screenOptions={{
+      headerStyle: {backgroundColor:"#841458"},
+      headerTintColor:'#fff',
+      headerTitleStyle:{fontWeight: "bold"}
+    }}
+  >
+    <Stack.Screen 
+          name="Home"
+          component={Home}
+          options={{title: "Home Page"}}
+    /> 
+  </Stack.Navigator>
+  )
+}
 const styles = StyleSheet.create({
 
 });
--- Screen/Home.js
+++ Screen/Home.js
@@ -5,7 +5,7 @@ import ImageSlider from 'react-native-image-slider'
 
 import FoodItems from "../Components/FoodItems"
 
-export default function App() {
+export default function Home() {
     let images = [
         require("../assets/bg.jpg"),
         require("../assets/rice.jpg"),
@@ -13,9 +13,7 @@ export default function App() {
     ]
     const foods = [
         {id: "1", image: require('../assets/steak.jpg'), name: "Steak", price: "20$", detail:  "A steak is a meat generally sliced across the muscle fibers, potentially including a bone. It is normally grilled, though can also be pan-fried. It is often grilled in an attempt to replicate the flavor of steak cooked over the glowing coals of an open fire." },
-    
         {id: "2", image: require('../assets/rice.jpg'), name: "Rice", price: "30$", detail:  "Rice is the seed of the grass species Oryza sativa or less commonly Oryza glaberrima. As a cereal grain, it is the most widely consumed staple food for a large part of the world's human population, especially in Asia and Africa." },
-    
         {id: "3", image: require('../assets/pizza.jpg'), name: "Pizza", price: "20$", detail:  "Pizza is a savory dish of Italian origin consisting of a usually round, flattened base of leavened wheat-based dough topped with tomatoes, cheese, and often various other ingredients, which is then baked at a high temperature, traditionally in a wood-fired oven. A small pizza is sometimes called a pizzetta." },
     
         {id: "4", image: require('../assets/eggs.jpg'), name: "Omlette", price: "20$", detail: "In cuisine, an omelette or omelet is a dish made from beaten eggs, fried with butter or oil in a frying pan (without stirring as in scrambled egg). It is quite common for the omelette to be folded around fillings such as cheese, chives, vegetables, mushrooms, meat (often ham or bacon), or some combination of the above." },
